a2dd7af0438c627b47dc278c1b1b2c9ade4cc040
thelistratov <thelistratov@gmail.com>
2024-10-26 12:24:16 +0700

    first_commit_init

---
+++ for_work.py
@@ -0,0 +1,11 @@
+class Man:
+    def __init__(self):
+        self.strengh = 25
+
+    def __str__(self):
+        print('У {} столько сил'.format(fink_igor.strengh))
+
+
+fink_igor = Man()
+
+fink_igor.strengh += 10
