dfb18ba1bd9f9f310ecf4f28faa03cc1ed1bcdfc
thelistratov <thelistratov@gmail.com>
2024-10-26 13:08:13 +0700

    add_code_for_lesson

--- for_work.py
+++ for_work.py
@@ -1,13 +1,34 @@
 class Man:
-    def __init__(self):
-        self.strengh = 25
+    def __init__(self, name):
+        self.name = name
+        self.fullness = 50
+        self.food = 50
+        self.money = 200
 
     def __str__(self):
-        print('У {} столько сил'.format(fink_igor.strengh))
+        return 'Я - {}, сытость {}, еды осталось {}, денег осталось {}'.format(
+            self.name, self.fullness, self.food, self.money)
+
+    def eat(self):
+        if self.food > 10:
+            print('{} поел'.format(self.name))
+            self.fullness += 10
+            self.food -= 10
+            self.money -= 10
+        else:
+            print('{} нет еды'.format(self.name))
+    def work(self):
+        print('{} сходил на работу'.format(self.name))
+        self.money += 50
+        self.fullness -= 10
+
+    def play_DOTS(self):
+        print('{} поиграл в DOTA2'.format(self.name))
+        self.fullness -= 10
 
 
-fink_igor = Man()
-
-fink_igor.strengh += 10
-
-print('test')
+vasya = Man(name='Вася')
+for day in range(1,21):
+    print('========================= день {} ===================='.format(day))
+    vasya.act()
+    print()
