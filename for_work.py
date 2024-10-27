58e2ed9ac236e7b13c64d9d03b6bc678675828b8
thelistratov_mac <thelistratov@gmail.com>
2024-10-27 14:23:49 +0700

    commit_for_mac_win_error

--- for_work.py
+++ for_work.py
@@ -1,38 +1,92 @@
+
+from random import randint
+
+
+#TODO
+# import sys
+# from termcolor import colored, cprint
+
+
+
+
 class Man:
     def __init__(self, name):
         self.name = name
         self.fullness = 50
         self.food = 50
         self.money = 200
+        self.house = None
 
     def __str__(self):
         return 'Я - {}, сытость {}, еды осталось {}, денег осталось {}'.format(
             self.name, self.fullness, self.food, self.money)
 
     def eat(self):
-        if self.food > 10:
+        if self.food >= 10:
             print('{} поел'.format(self.name))
             self.fullness += 10
             self.food -= 10
             self.money -= 10
         else:
-            print('{} нет еды'.format(self.name))
+            print('У {} нет еды, Пора в магазин'.format(self.name))
+            self.go_shop()
+
     def work(self):
         print('{} сходил на работу'.format(self.name))
         self.money += 50
         self.fullness -= 10
 
-    def play_DOTS(self):
+    def go_shop(self):
+        if self.money >= 50:
+            print('{} сходил в магазин'.format(self.name))
+            self.food += 50
+            self.money -= 50
+        else:
+            print('У {} кончились деньги, пора Васи сходить на работу'.format(self.name))
+            self.work()
+
+    def play_dota2(self):
         print('{} поиграл в DOTA2'.format(self.name))
         self.fullness -= 10
+        self.money -= 10
+
+    def act(self):
+        if self.fullness <= 0:
+            print('{} умер'.format(self.name))
+            return
+        dice = randint(1, 6)
+        if self.fullness < 20:
+            self.eat()
+        elif self.food <= 10:
+            self.go_shop()
+        elif self.money <= 30:
+            self.work()
+        elif dice == 1:
+            self.work()
+        elif dice == 2:
+            self.eat()
+        else:
+            self.play_dota2()
+
+    def go_house(self, house):
+        self.house = house
+        print('{} заехал в дом'.format(self.name))
+        self.fullness -= 10
+
+class House:
+    def __init__(self):
+        self.food = 0
+        self.money = 50
 
 
-vasya = Man(name='Вася')
-for day in range(1,21):
+
+
+beavis = Man(name='Beavis')
+batthead = Man(name='Batthead')
+for day in range(1, 410):
     print('========================= день {} ===================='.format(day))
-    vasya.act()
-    print()
+    beavis.act()
+    batthead.act()
+    print(beavis)
+    print(batthead)
 
-print('Тестовый комит с работы')
-print('Тестовый комит с дома, с user.name thelistratov_home')
-print('Тестовый комит с ноутбука, с user.name thelistratov_mac')
